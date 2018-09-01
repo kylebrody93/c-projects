5e0d08a4fdb5eefde3bc68be816947a5cfb46d09
kylebrody93 <41490305+kylebrody93@users.noreply.github.com>
2018-08-31 19:17:00 -0700

    Update secondTest.c

--- secondTest.c
+++ secondTest.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
-
 int main(int argc, char **argv)
 {
 	printf("type your first and last name!\n");
-		int a,b,c,d,e,f;
-		char input[20];
-		char input01[20];
+	int a,b,c,d,e,f;
+	char input[20];
+	char input01[20];
 	scanf("%s", input);
 	scanf("%s", input01);
 	printf("hello %s ", input);
 	printf("%s. please enter two numbers you would like to multiply.", input01);
 	scanf("%d", &a);
 	scanf("%d", &b);
-		c=a*b;
+	c=a*b;
 	printf("%d ", c);
 	printf("well %s %s... thats all for now!", input, input01);
 	return 0;
