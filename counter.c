3ef966ebadede9a0653f8745533d750b0691cd44
kylebrody93 <41490305+kylebrody93@users.noreply.github.com>
2018-09-14 14:24:07 -0700

    Update counter.c

--- counter.c
+++ counter.c
@@ -8,6 +8,7 @@ int main(void)
 	int count;
 	//-- LAST = the variable for the number that the counter will stop at. represents the number of times the user wants the program to count.
 	int LAST;
+	int factorial;
 
 for( ; ;) {
 	//-- asks how many times the user wants to count
@@ -15,208 +16,459 @@ for( ; ;) {
 	//-- scans for user input to determine how many times the program will count
 	scanf("%d", &LAST);
 	//-- asks what the user wants to count by
-	printf("how do you want to count? by 1, 2's, 3's, or 5's?");
+	printf("how do you want to count? by 1, 2's, 3's, or 5's");
 	//--scans for user input to determine what to count by
 	scanf("%d", &countby);
 	
 	//-- this tree begins if the user wants to count two times
+	 
 	if(LAST == 2 && countby == 1){
 		for (count=0; count<LAST ;)
 			printf("%d\n\n", count += 1);
+			printf("Calculating Factorials\n");
+			
+			for ( factorial = 1, count = 1; count <= 2;
+								factorial *= count++);
+								
+			printf("%d! is %d.\n", LAST, factorial);
 	}
 			//--this else if counts by two twice
 			else
 				if(LAST == 2 && countby ==2){
 					for (count=0; count<2*LAST ;)
 						printf("%d\n", count += 2);
+						printf("Calculating Factorials\n");
+						
+						for ( factorial = 1, count = 1; count <= 2;
+											factorial *= count++);
+											
+						printf("%d! is %d.\n", LAST, factorial);
 				}
 				//--this else if counts by three twice
 				else
 				if(LAST == 2 && countby ==3){
 					for (count=0; count<3*LAST ;)
 						printf("%d\n", count += 3); 
+						printf("Calculating Factorials\n");
+						
+						for ( factorial = 1, count = 1; count <= 2;
+											factorial *= count++);
+											
+						printf("%d! is %d.\n", LAST, factorial);
 				}
 				//--this else if counts by five twice
 						else
 							if(LAST == 2 && countby ==5){
 								for (count=0; count<5*LAST ;)
 									printf("%d\n", count += 5);
-							}
+									printf("Calculating Factorials\n");
+						
+									for ( factorial = 1, count = 1; count <= 2;
+														factorial *= count++);
+											
+									printf("%d! is %d.\n", LAST, factorial);
+										}
+							else 
+								
 	//-- this tree begins if the user wants to count three times
 	if(LAST == 3 && countby ==1){
 		for (count=0; count<LAST ;)
 			printf("%d\n", count += 1);
+			
+			printf("Calculating Factorials\n");
+						
+			for ( factorial = 1, count = 1; count <= 3;
+								factorial *= count++);
+											
+			printf("%d! is %d.\n", LAST, factorial);
 	}
 	//--this else if counts by two thrice
 			else
 				if(LAST == 3 && countby ==2){
 					for (count=0; count<2*LAST ;)
 						printf("%d\n", count += 2);
+						
+						printf("Calculating Factorials\n");
+						
+						for ( factorial = 1, count = 1; count <= 3;
+								factorial *= count++);
+											
+						printf("%d! is %d.\n", LAST, factorial);
 				}
 				//--this else if counts by three thrice
 				else
 				if(LAST == 3 && countby ==3){
 					for (count=0; count< 3*LAST ;)
 						printf("%d\n", count += 3); 
+						
+						printf("Calculating Factorials\n");
+						
+						for ( factorial = 1, count = 1; count <= 3;
+								factorial *= count++);
+											
+						printf("%d! is %d.\n", LAST, factorial);
 				}
 				//--this else if counts by four thrice
 				else
 					if(LAST == 3 && countby ==5){
 						for (count=0; count<5*LAST ;)
 							printf("%d\n", count += 5);
+							
+							printf("Calculating Factorials\n");
+						
+							for ( factorial = 1, count = 1; count <= 3;
+								factorial *= count++);
+											
+							printf("%d! is %d.\n", LAST, factorial);
 					}
 					
 	//-- this tree begins if the user wants to count four times				
 	if(LAST == 4 && countby == 1){
 		for (count=0; count<LAST ;)
 			printf("%d\n", count += 1);
+			
+			printf("Calculating Factorials\n");
+						
+			for ( factorial = 1, count = 1; count <= 4;
+								factorial *= count++);
+											
+			printf("%d! is %d.\n", LAST, factorial);
 	}
 			//--this else if counts by two four times
 			else
 				if(LAST == 4 && countby ==2){
 					for (count=0; count<2*LAST ;)
 						printf("%d\n", count += 2);
+						
+						printf("Calculating Factorials\n");
+						
+						for ( factorial = 1, count = 1; count <= 4;
+											factorial *= count++);
+											
+						printf("%d! is %d.\n", LAST, factorial);
 				}
 				//--this else if counts by three four times
 				else
 				if(LAST == 4 && countby ==3){
 					for (count=0; count<3*LAST ;)
 						printf("%d\n", count += 3); 
+						
+						printf("Calculating Factorials\n");
+						
+						for ( factorial = 1, count = 1; count <= 4;
+											factorial *= count++);
+											
+						printf("%d! is %d.\n", LAST, factorial);
 				}
 				//--this else if counts by five four times
 						else
 					if(LAST == 4 && countby ==5){
 						for (count=0; count<5*LAST ;)
 							printf("%d\n", count += 5);
+							
+							printf("Calculating Factorials\n");
+						
+							for ( factorial = 1, count = 1; count <= 4;
+												factorial *= count++);
+											
+							printf("%d! is %d.\n", LAST, factorial);
 					}
 					
 	//-- this tree begins if the user wants to count five times				
 	if(LAST == 5 && countby == 1){
 		for (count=0; count<LAST ;)
 			printf("%d\n", count += 1);
+			
+			printf("Calculating Factorials\n");
+						
+			for ( factorial = 1, count = 1; count <= 5;
+								factorial *= count++);
+											
+			printf("%d! is %d.\n", LAST, factorial);
 	}
 			//--this else if counts by two five times
 			else
 				if(LAST == 5 && countby ==2){
 					for (count=0; count<2*LAST ;)
 						printf("%d\n", count += 2);
+						//factorial calculation
+						printf("Calculating Factorials\n");
+						
+						for ( factorial = 1, count = 1; count <= 5;
+											factorial *= count++);
+											
+						printf("%d! is %d.\n", LAST, factorial);
 				}
 				//--this else if counts by three five times
 				else
 					if(LAST == 5 && countby ==3){
 						for (count=0; count<3*LAST ;)
 							printf("%d\n", count += 3); 
+							//factorial calculation
+							printf("Calculating Factorials\n");
+						
+							for ( factorial = 1, count = 1; count <= 5;
+												factorial *= count++);
+											
+							printf("%d! is %d.\n", LAST, factorial);
 					}
 					//--this else if counts by five five times
 					else
 						if(LAST == 5 && countby ==5){
 							for (count=0; count<5*LAST ;)
 								printf("%d\n", count += 5);
+								//factorial calculation
+								printf("Calculating Factorials\n");
+						
+								for ( factorial = 1, count = 1; count <= 5;
+													factorial *= count++);
+											
+								printf("%d! is %d.\n", LAST, factorial);
 						}
 					
 	//-- this tree begins if the user wants to count six times				
 	if(LAST == 6 && countby ==1){
 		for (count=0; count<LAST ;)
 			printf("%d\n", count += 1);
+			//factorial calculation
+			printf("Calculating Factorials\n");
+
+			for ( factorial = 1, count = 1; count <= 6;
+					factorial *= count++);
+														
+			printf("%d! is %d.\n", LAST, factorial);
 	}
 			else
 				if(LAST == 6 && countby ==2){
 					for (count=0; count<2*LAST ;)
 						printf("%d\n", count += 2);
+						//factorial calculation
+						printf("Calculating Factorials\n");
+
+						for ( factorial = 1, count = 1; count <= 6;
+								factorial *= count++);
+																	
+						printf("%d! is %d.\n", LAST, factorial);
 				}
 				else
 				if(LAST == 6 && countby ==3){
 					for (count=0; count<3*LAST ;)
 						printf("%d\n", count += 3); 
+						//factorial calculation
+						printf("Calculating Factorials\n");
+
+						for ( factorial = 1, count = 1; count <= 6;
+								factorial *= count++);
+																	
+						printf("%d! is %d.\n", LAST, factorial);
 				}
 						else
 					if(LAST == 6 && countby ==5){
 						for (count=0; count<5*LAST ;)
 							printf("%d\n", count += 5);
+							//factorial calculation
+							printf("Calculating Factorials\n");
+
+							for ( factorial = 1, count = 1; count <= 6;
+									factorial *= count++);
+																		
+							printf("%d! is %d.\n", LAST, factorial);
 					}
 					
 	//-- this tree begins if the user wants to count seven times				
 	if(LAST == 7 && countby ==1){
 		for (count=0; count<LAST ;)
 			printf("%d\n", count += 1);
+			//factorial calculation
+			printf("Calculating Factorials\n");
+
+			for ( factorial = 1, count = 1; count <= 7;
+					factorial *= count++);
+														
+			printf("%d! is %d.\n", LAST, factorial);
 	}
 			else
 				if(LAST == 7 && countby ==2){
 					for (count=0; count<2*LAST ;)
 						printf("%d\n", count += 2);
+						//factorial calculation
+						printf("Calculating Factorials\n");
+
+						for ( factorial = 1, count = 1; count <= 7;
+								factorial *= count++);
+																	
+						printf("%d! is %d.\n", LAST, factorial);
 				}
 				else
 				if(LAST == 7 && countby ==3){
 					for (count=0; count<3*LAST ;)
 						printf("%d\n", count += 3); 
+						//factorial calculation
+						printf("Calculating Factorials\n");
+
+						for ( factorial = 1, count = 1; count <= 7;
+								factorial *= count++);
+																	
+						printf("%d! is %d.\n", LAST, factorial);
 				}
 						else
 					if(LAST == 7 && countby ==5){
 						for (count=0; count<5*LAST ;)
 							printf("%d\n", count += 5);
+							//factorial calculation
+							printf("Calculating Factorials\n");
+
+							for ( factorial = 1, count = 1; count <= 7;
+									factorial *= count++);
+																		
+							printf("%d! is %d.\n", LAST, factorial);
 					}
 	
 	//-- this tree begins if the user wants to count eight times
 	if(LAST == 8 && countby ==1 ){
 		for (count=0; count<LAST ;)
 			printf("%d\n", count += 1);
+			//factorial calculation
+			printf("Calculating Factorials\n");
+
+			for ( factorial = 1, count = 1; count <= 8;
+					factorial *= count++);
+														
+			printf("%d! is %d.\n", LAST, factorial);
 	}
 			else
 				if(LAST == 8 && countby ==2){
 					for (count=0; count<2*LAST ;)
 						printf("%d\n", count += 2);
+						//factorial calculation
+						printf("Calculating Factorials\n");
+
+						for ( factorial = 1, count = 1; count <= 8;
+								factorial *= count++);
+																	
+						printf("%d! is %d.\n", LAST, factorial);
 				}
 				else
 				if(LAST == 8 && countby ==3){
 					for (count=0; count<3*LAST ;)
 						printf("%d\n", count += 3); 
+						//factorial calculation
+						printf("Calculating Factorials\n");
+
+						for ( factorial = 1, count = 1; count <= 8;
+								factorial *= count++);
+																	
+						printf("%d! is %d.\n", LAST, factorial);
 				}
 						else
 					if(LAST == 8 && countby ==5){
 						for (count=0; count<5*LAST ;)
 							printf("%d\n", count += 5);
+							//factorial calculation
+							printf("Calculating Factorials\n");
+
+							for ( factorial = 1, count = 1; count <= 8;
+									factorial *= count++);
+																		
+							printf("%d! is %d.\n", LAST, factorial);
 					}
 					
 	//-- this tree begins if the user wants to count nine times				
 	if(LAST == 9 && countby == 1){
 		for (count=0; count<LAST ;)
 			printf("%d\n", count += 1);
+			//factorial calculation
+			printf("Calculating Factorials\n");
+
+			for ( factorial = 1, count = 1; count <= 9;
+					factorial *= count++);
+														
+			printf("%d! is %d.\n", LAST, factorial);
 	}
 			else
 				if(LAST == 9 && countby ==2){
 					for (count=0; count<2*LAST ;)
 						printf("%d\n", count += 2);
+						//factorial calculation
+						printf("Calculating Factorials\n");
+
+						for ( factorial = 1, count = 1; count <= 9;
+								factorial *= count++);
+																				
+						printf("%d! is %d.\n", LAST, factorial);
 				}
 				else
 				if(LAST == 9 && countby ==3){
 					for (count=0; count<3*LAST ;)
 						printf("%d\n", count += 3); 
+						//factorial calculation
+						printf("Calculating Factorials\n");
+
+						for ( factorial = 1, count = 1; count <= 9;
+								factorial *= count++);
+																				
+						printf("%d! is %d.\n", LAST, factorial);
 				}
 						else
 					if(LAST == 9 && countby ==5){
 						for (count=0; count<5*LAST ;)
 							printf("%d\n", count += 5);
+							//factorial calculation
+							printf("Calculating Factorials\n");
+
+							for ( factorial = 1, count = 1; count <= 9;
+									factorial *= count++);
+																					
+							printf("%d! is %d.\n", LAST, factorial);
 					}
 	
 	//-- this tree begins if the user wants to count ten times
 	if(LAST == 10 && countby == 1){
 		for (count=0; count<LAST ;)
 			printf("%d\n", count += 1);
+			//factorial calculation
+			printf("Calculating Factorials\n");
+
+			for ( factorial = 1, count = 1; count <= 10;
+					factorial *= count++);
+																	
+			printf("%d! is %d.\n", LAST, factorial);
 	}
 			else
 				if(LAST == 10 && countby ==2){
 					for (count=0; count<2*LAST ;)
 						printf("%d\n", count += 2);
+						//factorial calculation
+						printf("Calculating Factorials\n");
+
+						for ( factorial = 1, count = 1; count <= 10;
+								factorial *= count++);
+																				
+						printf("%d! is %d.\n", LAST, factorial);
 				}
 				else
 				if(LAST == 10 && countby ==3){
 					for (count=0; count<3*LAST ;)
 						printf("%d\n", count += 3); 
+						//factorial calculation
+						printf("Calculating Factorials\n");
+
+						for ( factorial = 1, count = 1; count <= 10;
+								factorial *= count++);
+																				
+						printf("%d! is %d.\n", LAST, factorial);
 				}
 						else
 					if(LAST == 10 && countby ==5){
 						for (count=0; count<5*LAST ;)
 							printf("%d\n", count += 5);
+							//factorial calculation
+							printf("Calculating Factorials\n");
+
+							for ( factorial = 1, count = 1; count <= 10;
+									factorial *= count++);
+																					
+							printf("%d! is %d.\n", LAST, factorial);
 } }
 		
 	return 0;
