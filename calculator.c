f303afc90191e50bd66377119579d701dedb31f1
kylebrody93 <41490305+kylebrody93@users.noreply.github.com>
2018-09-19 18:27:13 -0700

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -18,40 +18,40 @@ int main(void)
 	
 	int result;
 	
-		printf("enter a number: \n\n");
-		scanf("%d", &num1);
+	printf("enter a number: \n\n");
+	scanf("%d", &num1);
 		
-		//--- user picks their intended operation ---
+	//--- user picks their intended operation ---
 		
-		printf("1- add 2- subtract 3- multiply 4- divide \n\n");
-		scanf("%d", &operation);
+	printf("1- add 2- subtract 3- multiply 4- divide \n\n");
+	scanf("%d", &operation);
 			
 		
 		
-		printf("enter your second number: \n\n");
-		scanf("%d", &num2);
+	printf("enter your second number: \n\n");
+	scanf("%d", &num2);
 		
-		//result is determined by the chosen operation
+	//result is determined by the chosen operation
 		
-			if(operation == 1){
-				result=num1+num2;
-				printf("%d \n\n", result);
-			}
-			else if(operation == 2){
-					result=num1-num2;
-					printf("%d \n\n", result);
-				}
+	if(operation == 1){
+		result=num1+num2;
+		printf("%d \n\n", result);
+	}
+	else if(operation == 2){
+			result=num1-num2;
+			printf("%d \n\n", result);
+		}
 				
-			else if(operation == 3){
-					result=num1*num2;
-					printf("%d \n\n", result);
-				}
+	else if(operation == 3){
+			result=num1*num2;
+			printf("%d \n\n", result);
+		}
+					
+	else if(operation == 4){
+			result=num1/num2;
+			printf("%d \n\n", result);
+		}
 							
-			else if(operation == 4){
-					result=num1/num2;
-					printf("%d \n\n", result);
-				}
-									
 		//here the user can continue to work with their first result. I used an infinite for loop to continue the MATH
 		
 	for( ; ; ) {	
